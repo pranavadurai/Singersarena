61b8a931d607ec9e256281f02f7b7805f0dad08b
Pranavadurai <pranavadurai@gmail.com>
2017-04-25 21:32:31 +0530

    mail basic layout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,8 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.preview_path
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
